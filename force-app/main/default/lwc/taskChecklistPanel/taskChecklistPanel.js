74c8131d7699d07bf75488affb4e2fccb18fc48c
Jason Auger <jason.auger@gmail.com>
2025-05-09 12:07:12 +0200

    changes to updating taskIdsToUpdate stack

--- force-app/main/default/lwc/taskChecklistPanel/taskChecklistPanel.js
+++ force-app/main/default/lwc/taskChecklistPanel/taskChecklistPanel.js
@@ -6,12 +6,15 @@ export default class TaskChecklistPanel extends LightningElement {
   //  Case record Id
   @api recordId;
 
-  @track taskList;
+  @track taskList = [];
   @track progress = 0;
   @track isSubmitBtnDisabled = false;
 
   taskIdsToUpdate = [];
 
+  /**
+   *
+   */
   connectedCallback() {
     getChecklistData({ caseId: this.recordId })
       .then((data) => {
@@ -23,19 +26,37 @@ export default class TaskChecklistPanel extends LightningElement {
       });
   }
 
-  handleCheckboxChange(event) {
-    const checkbox = event.target;
-
-    if (this.taskIdsToUpdate.includes(checkbox.name) && !checkbox.checked) {
-      this.taskIdsToUpdate.pop(checkbox.name);
-    } else if (
-      !this.taskIdsToUpdate.includes(checkbox.name) &&
-      checkbox.checked
-    ) {
-      this.taskIdsToUpdate.push(checkbox.name);
+  /**
+   *
+   */
+  handleUpdateTaskStatuses() {
+    //  Nothing is selected, so don't do anything
+    if (this.taskIdsToUpdate.length === 0) {
+      return;
     }
+
+    this.isSubmitBtnDisabled = true;
+
+    updateChecklist({
+      caseId: this.recordId,
+      taskIds: JSON.stringify(this.taskIdsToUpdate),
+    })
+      .then((result) => {
+        this.taskList = result;
+        this.calculateProgress();
+        //  We did the things, enable the Submit button and empty the update list
+        this.isSubmitBtnDisabled = false;
+        this.taskIdsToUpdate = [];
+      })
+      .catch((error) => {
+        console.error("Error updating and fetching data:", error);
+        this.isSubmitBtnDisabled = false;
+      });
   }
 
+  /**
+   *
+   */
   calculateProgress() {
     let totalTasksCompleted = 0;
     for (const task of this.taskList) {
@@ -47,6 +68,30 @@ export default class TaskChecklistPanel extends LightningElement {
     this.progress = (totalTasksCompleted / this.taskList.length) * 100;
   }
 
+  /**
+   *
+   * @param {*} event
+   */
+  handleCheckboxChange(event) {
+    const checkbox = event.target;
+
+    if (this.taskIdsToUpdate.includes(checkbox.name) && !checkbox.checked) {
+      //  Remove TaskId from list
+      this.taskIdsToUpdate = this.taskIdsToUpdate.filter(
+        (taskId) => taskId !== checkbox.name
+      );
+    } else if (
+      !this.taskIdsToUpdate.includes(checkbox.name) &&
+      checkbox.checked
+    ) {
+      //  Add TaskId to list
+      this.taskIdsToUpdate.push(checkbox.name);
+    }
+  }
+
+  /**
+   *
+   */
   handleUpdateTaskStatuses() {
     //  Nothing is selected, so don't do anything
     if (this.taskIdsToUpdate.length === 0) {
